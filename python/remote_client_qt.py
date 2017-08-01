839ca29f6cab38c5a9b07a82ab21da8e66f997f8
gercap <germanocapela@gmail.com>
2017-08-01 23:52:33 +0100

    added support for networked connection between local worker and plotter fragmentation is supported (MTU limitation)

--- python/remote_client_qt.py
+++ python/remote_client_qt.py
@@ -22,7 +22,7 @@
 # This block intends to be used with local_worker from gr-ofdm_tools
 # 
 # 
-
+import struct
 from plotter_base import *
 
 class remote_client_qt(plotter_base):
@@ -30,6 +30,7 @@ class remote_client_qt(plotter_base):
         plotter_base.__init__(self, blkname="remote_client_qt", label=label, *args)
         self.message_port_register_in(pmt.intern("pdus"));
         self.set_msg_handler(pmt.intern("pdus"), self.handler);
+        self.reasembled_frame = ''
 
         # set up curves
         curve = Qwt.QwtPlotCurve("PSD");
@@ -40,16 +41,33 @@ class remote_client_qt(plotter_base):
         self.curve_data = [([],[]), ([],[])];
 
     def handler(self, msg_pmt):
-        meta = pmt.to_python(pmt.car(msg_pmt))
+        #meta = pmt.to_python(pmt.car(msg_pmt))
+
         # Collect message, convert to Python format:
         msg = pmt.cdr(msg_pmt)
         # Convert to string:
         msg_str = "".join([chr(x) for x in pmt.u8vector_elements(msg)])
+        n_frags = struct.unpack('!B', msg_str[0])[0] #obtain number of fragments
+        frag_id = struct.unpack('!B', msg_str[1])[0] #obtain fragment number
+        msg_str = msg_str[2:] #grab fft data
 
-        fft_data = numpy.fromstring(msg_str, numpy.float32)
+        #print 'fragment', frag_id, 'of', n_frags, 'len', len(msg_str)
 
-        # pass data
-        self.curve_data[0] = (numpy.linspace(1,len(fft_data),len(fft_data)), fft_data);
+        if n_frags == 1: #single fragment
+            fft_data = numpy.fromstring(msg_str, numpy.float32)
+            # pass data
+            self.curve_data[0] = (numpy.linspace(1,len(fft_data),len(fft_data)), fft_data);
+            # trigger update
+            self.emit(QtCore.SIGNAL("updatePlot(int)"), 0)
 
-        # trigger update
-        self.emit(QtCore.SIGNAL("updatePlot(int)"), 0)
+        else: #multiple fragments situation
+            self.reasembled_frame += msg_str
+            if frag_id == n_frags - 1: #final fragment
+                fft_data = numpy.fromstring(self.reasembled_frame, numpy.float32)
+                # pass data
+                self.curve_data[0] = (numpy.linspace(1,len(fft_data),len(fft_data)), fft_data);
+                # trigger update
+                self.emit(QtCore.SIGNAL("updatePlot(int)"), 0)
+                self.reasembled_frame = ''
+            else:
+                pass
